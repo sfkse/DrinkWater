63a2db365f7ca5a32309474d072e240e13678315
sfkse <sfkse@users.noreply.github.com>
2021-04-12 19:13:48 +0300

    Update files

--- script.js
+++ script.js
@@ -1,37 +1,63 @@
 
-let cups = document.querySelectorAll('.cup')
+let cups = document.querySelectorAll('.cup-small')
 let remainedLiters = document.querySelector('#liters')
 let remainedSection = document.querySelector('.remained')
 let percentage = document.querySelector('.percentage')
 
+updateBigCup()
 cups.forEach((item, idx) => {
 
 
-    item.addEventListener('click', () => {
-
-        count(idx)
-
-        // console.log(document.querySelectorAll('.cup')[idx])
-        function count(idx) {
-
-            cups.forEach((item, idx2) => {
-                if (idx2 <= idx) {
-
-                    item.classList.add('full')
-                    percentage.textContent = (100 / 8) * idx + '%'
-                    percentage.style.height = `${(100 / 8) * idx}%`
-                    remainedLiters.textContent = 2 - (1 / 4) * idx + 'L'
-                    remained.style.height = `${2 - (1 / 4) * idx}%`
+    item.addEventListener('click', () => highliht(idx))
 
 
-                    console.log(percentage.textContent)
-                } else {
-                    item.classList.remove('full')
-                }
-
-            })
-
-
-        }
-    })
 })
+
+function highliht(idx) {
+    if (cups[idx].classList.contains('full') && !cups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+    cups.forEach((clickedItem, index) => {
+
+
+
+        if (index <= idx) {
+
+            clickedItem.classList.add('full')
+        }
+
+        else {
+
+            clickedItem.classList.remove('full')
+        }
+
+    })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = cups.length
+
+    if (fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
+}
+
+
